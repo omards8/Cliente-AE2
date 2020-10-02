68a6a6dbb31a84c073460b88b1280f04b76c19af
Omar <44298100+omards8@users.noreply.github.com>
2020-10-02 21:16:36 +0200

    AE2-1

---
+++ ae2_1.js
@@ -0,0 +1,23 @@
+// 1.a)  //
+let a = ["Ana", 2];
+let b = ["Javi", 5];
+let c = ["Roberto",7];
+let d = ["Vanessa", 10];
+
+console.log(a[0]);
+console.log(b[0]);
+console.log(c[0]);
+console.log(d[0]);
+// 1.b)  //
+if (a[1]%2==0) {
+    console.log(a[1]);
+}
+if (b[1]%2==0) {
+    console.log(b[1]);
+}
+if (c[1]%2==0) {
+    console.log(c[1]);
+}
+if (d[1]%2==0) {
+    console.log(d[1]);
+}
